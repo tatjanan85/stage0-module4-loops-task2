42776ddb29a16e5cd3c050df4e2df711e308e23f
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2023-12-26 15:20:40 +0300

    feat: add loop for negative values

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -4,9 +4,20 @@ public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
         int count = 0;
 
-        while (count <= multiplyByAndToInclusive) {
-            System.out.println(count * multiplyByAndToInclusive);
-            count++;
+        if (multiplyByAndToInclusive > 0) {
+            while (count <= multiplyByAndToInclusive) {
+                System.out.println(count * multiplyByAndToInclusive);
+                count++;
+            }
+        } else if (multiplyByAndToInclusive < 0) {
+            while (count >= multiplyByAndToInclusive) {
+                System.out.println(count * (-multiplyByAndToInclusive));
+                count--;
+            }
         }
+    }
+
+    public static void main(String[] args) {
+        new Multiplication().printMultiplied(-5);
     }
 }
